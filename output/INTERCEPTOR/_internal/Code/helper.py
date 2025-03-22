24a08d3e7e763a3bd739c4d1a56ac855e30801d6
Lucas Mehl <LucasMehl@outlook.com>
2025-03-22 14:27:18 -0300

    removing non required folders

--- output/INTERCEPTOR/_internal/Code/helper.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-def resource_path(relative_path):
-    """Get absolute path to resource, works for dev and for cx_freeze"""
-    base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
